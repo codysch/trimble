b46f875e7bc6f106d7633f07861b3ce94cff8513
Cody P Schafer <dev@codyps.com>
2022-03-08 11:39:03 -0500

    now with nvs!

--- carrot/src/main.rs
+++ carrot/src/main.rs
@@ -1,15 +1,7 @@
-// See
-// - https://github.com/esp-rs/esp32-hal/blob/master/examples/leds.rs
-// - https://github.com/esp-rs/esp-idf-hal/blob/master/examples/rmt_neopixel.rs
-
+extern crate alloc;
+use alloc::sync::Arc;
+use embedded_svc::storage::Storage;
 use log::info;
-
-use core::time::Duration;
-use embedded_hal::delay::blocking::DelayUs;
-use esp_idf_hal::delay::Ets;
-use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::rmt::config::TransmitConfig;
-use esp_idf_hal::rmt::{FixedLengthSignal, PinState, Pulse, Transmit};
 
 fn main() {
     esp_idf_sys::link_patches();
@@ -17,40 +9,16 @@ fn main() {
 
     info!("booted");
 
-    let peripherals = Peripherals::take().unwrap();
-    let led = peripherals
-        .pins
-        .gpio48
-        .into_output()
-        .expect("could not get led GPIO");
-    let channel = peripherals.rmt.channel0;
-    let config = TransmitConfig::new().clock_divider(1);
-    let mut tx = Transmit::new(led, channel, &config).unwrap();
+    let nvs = Arc::new(esp_idf_svc::nvs::EspDefaultNvs::new().unwrap());
+    let mut storage =
+        esp_idf_svc::nvs_storage::EspNvsStorage::new_default(nvs, "carrot", true).unwrap();
 
-    let rgbs = [0xff0000, 0xffff00, 0x00ffff, 0x00ff00, 0xa000ff];
+    let mut count: u32 = storage.get("count").unwrap().unwrap_or(0);
+    info!("initial count: {}", count);
     loop {
-        info!("outer");
-        for rgb in rgbs {
-            info!("inner");
-
-            let ticks_hz = tx.counter_clock().unwrap();
-            let t0h = Pulse::new_with_duration(ticks_hz, PinState::High, &ns(350)).unwrap();
-            let t0l = Pulse::new_with_duration(ticks_hz, PinState::Low, &ns(800)).unwrap();
-            let t1h = Pulse::new_with_duration(ticks_hz, PinState::High, &ns(700)).unwrap();
-            let t1l = Pulse::new_with_duration(ticks_hz, PinState::Low, &ns(600)).unwrap();
-
-            let mut signal = FixedLengthSignal::<24>::new();
-            for i in 0..24 {
-                let bit = 2_u32.pow(i) & rgb != 0;
-                let (high_pulse, low_pulse) = if bit { (t1h, t1l) } else { (t0h, t0l) };
-                signal.set(i as usize, &(high_pulse, low_pulse)).unwrap();
-            }
-            tx.start_blocking(&signal).unwrap();
-            Ets.delay_ms(1000).unwrap();
-        }
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        count += 1;
+        info!("count: {}", count);
+        storage.put("count", &count).unwrap();
     }
-}
-
-fn ns(nanos: u64) -> Duration {
-    Duration::from_nanos(nanos)
 }
